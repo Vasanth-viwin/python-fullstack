0bc43f3bf0dc8ffae293c610a789d27964eb5f8e
Vasanth-viwin <vasanthviwin@gmail.com>
2026-07-28 10:41:24 +0000

    html

---
+++ js/script.js
@@ -0,0 +1,4 @@
+alert("welcome to NRIIT Mearning Management System")
+let heading = document.getElementById("welcome");
+heading.innerHTML = "welcome Future softwere Engineers"
+console.log("heading element:",heading)
---
+++ js/test.js
@@ -0,0 +1,13 @@
+console.log("hello world")
+let student_name = "vasanth"
+console.log("student name is:" + student_name)
+let student_age =21
+let course_name="pthon full stak"
+console.log("student age is:"+ student_age, "course_name is:" + course_name)
+let fee = 10000
+let discount =10
+let discounted_fee = fee - (fee * discount / 100)
+console.log("Discounted fee is:" + discounted_fee)
+for(let i=1; i<=5; i++){
+    console.log("iteration number")
+}
